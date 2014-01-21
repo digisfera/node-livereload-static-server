2dc3cbe20b4db11b27283c058776d83cf6b6a7d0
Manuel Cabral <m.cabral@digisfera.pt>
2014-01-21 19:58:44 +0000

    Converted module to javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+var connect = require('connect');
+var path = require('path');
+var tinyLr = require('tiny-lr');
+
+module.exports = function(base, port, lrPort) {
+  port = port || 8080;
+  lrPort = lrPort || 35729;
+  connect()
+    .use(require('connect-disable-304')())
+    .use(require('connect-livereload')())
+    .use(connect.static(path.resolve(base)))
+    .use(connect.directory(path.resolve(base)))
+    .listen(port);
+
+  lrServer = tinyLr();
+  lrServer.listen(lrPort);
+
+  return function(file) {
+    lrServer.changed({ body: { files: [file] }});
+  };
+}
